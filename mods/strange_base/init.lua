9d7c33a828f1cfe4171ec7cad81762157fb03696
Eric K Rustrum <rustrume184@svusd.net>
2023-05-16 08:23:09 -0700

    Added in more stuff.
    Now including more stuff.

--- mods/strange_base/init.lua
+++ mods/strange_base/init.lua
@@ -1,9 +1,50 @@
+-- item registration
+
+core.register_item("strange_base:reinforcement", {
+	description = "Orang",
+	tiles = {"node19.png"},
+})
+
+
+
 -- node registration
 
 core.register_node("strange_base:orang", {
 	description = "Orang",
 	tiles = {"node19.png"},
 	groups = {breakable = 0},
+	drop = {}
+})
+
+core.register_node("strange_base:orang_grass_1", {
+	description = "Orang Grass",
+	tiles = {"node34.png"},
+	groups = {breakable = 0},
+	paramtype2 = "facedir",
+	drop = {}
+})
+
+core.register_node("strange_base:orang_grass_2", {
+	description = "Orang Grass",
+	tiles = {"node33.png"},
+	groups = {breakable = 0},
+	paramtype2 = "facedir",
+	drop = {}
+})
+
+core.register_node("strange_base:orang_grass_3", {
+	description = "Orang Grass",
+	tiles = {"node35.png"},
+	groups = {breakable = 0},
+	paramtype2 = "facedir",
+	drop = {}
+})
+
+core.register_node("strange_base:orang_grass_4", {
+	description = "Orang Grass",
+	tiles = {"node36.png"},
+	groups = {breakable = 0},
+	paramtype2 = "facedir",
 	drop = {}
 })
 
@@ -14,6 +55,7 @@ core.register_node("strange_base:orang_seed_pod", {
 	after_dig_node = function(pos, oldnode, oldmetadata, digger)
 		minetest.set_node(pos, {name = "strange_base:orang_seed_pod_dead"})
 	end,
+	node_dig_prediction = "strange_base:orang_seed_pod",
 	drop = {}
 })
 
@@ -31,6 +73,7 @@ core.register_node("strange_base:hyte", {
 	after_dig_node = function(pos, oldnode, oldmetadata, digger)
 		minetest.set_node(pos, {name = "strange_base:hyte_1"})
 	end,
+	node_dig_prediction = "strange_base:hyte_1",
 	drop = {}
 })
 
@@ -41,6 +84,7 @@ core.register_node("strange_base:hyte_1", {
 	after_dig_node = function(pos, oldnode, oldmetadata, digger)
 		minetest.set_node(pos, {name = "strange_base:hyte_2"})
 	end,
+	node_dig_prediction = "strange_base:hyte_2",
 	drop = {}
 })
 
@@ -51,6 +95,7 @@ core.register_node("strange_base:hyte_2", {
 	after_dig_node = function(pos, oldnode, oldmetadata, digger)
 		minetest.set_node(pos, {name = "strange_base:hyte_3"})
 	end,
+	node_dig_prediction = "strange_base:hyte_3",
 	drop = {}
 })
 
@@ -61,6 +106,7 @@ core.register_node("strange_base:hyte_3", {
 	after_dig_node = function(pos, oldnode, oldmetadata, digger)
 		minetest.set_node(pos, {name = "strange_base:hyte_4"})
 	end,
+	node_dig_prediction = "strange_base:hyte_4",
 	drop = {}
 })
 
@@ -71,6 +117,51 @@ core.register_node("strange_base:hyte_4", {
 	after_dig_node = function(pos, oldnode, oldmetadata, digger)
 		minetest.set_node(pos, {name = "strange_base:orang"})
 	end,
+	node_dig_prediction = "strange_base:orang",
+	drop = {}
+})
+
+core.register_node("strange_base:reinf_orang", {
+	description = "Reinforcement",
+	tiles = {"node20.png"},
+	groups = {breakable = 1},
+	after_dig_node = function(pos, oldnode, oldmetadata, digger)
+		minetest.set_node(pos, {name = "strange_base:reinf_orang_1"})
+	end,
+	node_dig_prediction = "strange_base:reinf_orang_1",
+	drop = {}
+})
+
+core.register_node("strange_base:reinf_orang_1", {
+	description = "Cracked Reinforcement",
+	tiles = {"node22.png"},
+	groups = {breakable = 1},
+	after_dig_node = function(pos, oldnode, oldmetadata, digger)
+		minetest.set_node(pos, {name = "strange_base:reinf_orang_2"})
+	end,
+	node_dig_prediction = "strange_base:reinf_orang_2",
+	drop = {}
+})
+
+core.register_node("strange_base:reinf_orang_2", {
+	description = "Cracked Reinforcement",
+	tiles = {"node23.png"},
+	groups = {breakable = 1},
+	after_dig_node = function(pos, oldnode, oldmetadata, digger)
+		minetest.set_node(pos, {name = "strange_base:reinf_orang_3"})
+	end,
+	node_dig_prediction = "strange_base:reinf_orang_3",
+	drop = {}
+})
+
+core.register_node("strange_base:reinf_orang_3", {
+	description = "Pulverized Reinforcement",
+	tiles = {"node24.png"},
+	groups = {breakable = 1},
+	after_dig_node = function(pos, oldnode, oldmetadata, digger)
+		minetest.set_node(pos, {name = "strange_base:orang"})
+	end,
+	node_dig_prediction = "strange_base:orang",
 	drop = {}
 })
 
@@ -108,18 +199,65 @@ minetest.register_decoration({
 	place_on = "strange_base:orang",
 	flags = "place_center_x, place_center_z",
 	rotation = "random",
-	fill_ratio = 0.05,
+	fill_ratio = 0.001,
 	sidelen = 9,
 	max_y = 4,
 	min_y = -4
 })
 
 minetest.register_decoration({
-	decoration = "strange_base:seed_pod",
-	deco_type = "simple",
+	decoration = "strange_base:grassy_patch_1",
+	deco_type = "schematic",
+	schematic = minetest.get_modpath("strange_base").."/schems/grassy_patch_1.mts",
 	place_on = "strange_base:orang",
 	flags = "place_center_x, place_center_z",
 	rotation = "random",
-	max_y = 4,
-	min_y = -4
+	fill_ratio = 0.0001,
+	sidelen = 11,
+	max_y = 1,
+	min_y = -1
 })
+
+minetest.register_decoration({
+	decoration = "strange_base:grassy_patch_2",
+	deco_type = "schematic",
+	schematic = minetest.get_modpath("strange_base").."/schems/grassy_patch_2.mts",
+	place_on = "strange_base:orang",
+	flags = "place_center_x, place_center_z",
+	rotation = "random",
+	fill_ratio = 0.0001,
+	sidelen = 11,
+	max_y = 1,
+	min_y = -1
+})
+
+
+
+-- register ores
+minetest.register_ore({
+	ore_type = "blob",
+	ore = "strange_base:orang_seed_pod",
+	wherein = "strange_base:orang",
+	max_y = 8,
+	min_y = -8,
+	clust_scarcity = 5*5*5,
+	clust_size = 2,
+})
+
+
+
+minetest.register_on_joinplayer(function(player)
+	player:set_sun({
+		visible = true
+	})
+
+	player:set_moon({
+		visible = false
+	})
+
+	player:set_stars({
+		visible = false
+	})
+
+	player:override_day_night_ratio(1)
+end)
